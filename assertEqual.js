5afeb05fe5e836641675a1f0064884eea70654a0
JoshGrant5 <joshgg@icloud.com>
2020-09-14 21:28:45 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`) :
+  console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual('Josh', 'josh');
+assertEqual('Josh', 'Josh');
+
+
