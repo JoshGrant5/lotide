752146b990c069f654525e67b0fc636845d04726
JoshGrant5 <joshgg@icloud.com>
2020-09-15 22:47:50 +0000

    Implement flatten

---
+++ flatten.js
@@ -0,0 +1,31 @@
+const eqArrays = (array1, array2) => {
+  let equal = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      equal = false;
+    }
+  }
+  return (equal ? true : false);
+};
+ 
+const assertArraysEqual = (array1, array2) => {
+  eqArrays(array1, array2) ? console.log(`😊😊😊 Assertion Passed: ${array1} === ${array2}`) :
+    console.log(`🤬🤬🤬 Assertion Failed: ${array1} !== ${array2}`);
+};
+
+// Return a "flattened" array 
+const flatten = array => {
+  let flattened = [];
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      for (let i of item) {
+        flattened.push(i);
+      }
+    } else {
+      flattened.push(item);
+    }
+  }
+  return flattened;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
