a30ca6d0027e62dc43cece0de860483f7d79a068
JoshGrant5 <joshgg@icloud.com>
2020-09-16 20:27:51 +0000

    removed other functions

--- flatten.js
+++ flatten.js
@@ -1,18 +1,3 @@
-const eqArrays = (array1, array2) => {
-  let equal = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || array1.length !== array2.length) {
-      equal = false;
-    }
-  }
-  return (equal ? true : false);
-};
- 
-const assertArraysEqual = (array1, array2) => {
-  eqArrays(array1, array2) ? console.log(`😊😊😊 Assertion Passed: ${array1} === ${array2}`) :
-    console.log(`🤬🤬🤬 Assertion Failed: ${array1} !== ${array2}`);
-};
-
 // Return a "flattened" array 
 const flatten = array => {
   let flattened = [];
