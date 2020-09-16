48a91c2860a7599804c98c43cdb947a525d18be2
JoshGrant5 <joshgg@icloud.com>
2020-09-16 00:35:39 +0000

    Refactored eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,13 @@
 const eqArrays = (array1, array2) => {
-  let equal = true;
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || array1.length !== array2.length) {
-      equal = false;
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return (equal ? true : false);
+  return true;
 };
  
 const assertArraysEqual = (array1, array2) => {
