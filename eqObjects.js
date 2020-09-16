c8d1ca6be74dfa47c8c59bc70358ab566864a454
JoshGrant5 <joshgg@icloud.com>
2020-09-16 20:15:28 +0000

    Lint clean up

--- eqObjects.js
+++ eqObjects.js
@@ -1,8 +1,3 @@
-const assertEqual = (actual, expected) => {
-  actual === expected ? console.log(`😊😊😊 Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
@@ -17,21 +12,21 @@ const eqArrays = (array1, array2) => {
 
 // Take in two objects and return true or false, based on a perfect match
 const eqObjects = (object1, object2) => {
-   if (Object.keys(object1).length !== Object.keys(object2).length) { 
-     return false;
-   } else {
-     for (let key in object1) {
-       if (!Object.keys(object2).includes(key)) { // are the keys the same for both objects?
-         return false;
-       }
-       if (Array.isArray(object1[key])) { // is the value an array?
-         if (!eqArrays(object1[key], object2[key])) { // if so, does this array match with the value of object2?
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let key in object1) {
+      if (!Object.keys(object2).includes(key)) { // are the keys the same for both objects?
+        return false;
+      }
+      if (Array.isArray(object1[key])) { // is the value an array?
+        if (!eqArrays(object1[key], object2[key])) { // if so, does this array match with the value of object2?
           return false;
-         }
-       }
-     }
-   }
-   return true;
+        }
+      }
+    }
+  }
+  return true;
 };
 
 // Test code
