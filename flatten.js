729767970949e0c952e8bb5f749da9fa2a130aed
JoshGrant5 <joshgg@icloud.com>
2020-09-16 00:31:38 +0000

    Refactored with Array .concat()

--- flatten.js
+++ flatten.js
@@ -17,15 +17,10 @@ const assertArraysEqual = (array1, array2) => {
 const flatten = array => {
   let flattened = [];
   for (let item of array) {
-    if (Array.isArray(item)) {
-      for (let i of item) {
-        flattened.push(i);
-      }
-    } else {
-      flattened.push(item);
-    }
+    Array.isArray(item) ? flattened = flattened.concat(item) : flattened.push(item);
   }
   return flattened;
 }
 
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
