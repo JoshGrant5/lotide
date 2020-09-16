d5010c6388d07ec673e8ea2a4f2c5d53c410310b
JoshGrant5 <joshgg@icloud.com>
2020-09-16 22:12:39 +0000

    Added another line of test code

--- flatten.js
+++ flatten.js
@@ -7,5 +7,7 @@ const flatten = array => {
   return flattened;
 }
 
+// Test code
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, [2, 3, 4], [5, 6, 7, 8]])); // => [1, 2, 3, 4, 5, 6, 7, 8]
 
