45f6c681e4b6cfe5ec860d8659e0b952d9b0e72b
JoshGrant5 <joshgg@icloud.com>
2020-09-17 20:21:11 +0000

    Implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+ 
+const assertArraysEqual = (array1, array2) => {
+  const inspect = require('util').inspect; // allows us to view the arrays in our console.log
+  eqArrays(array1, array2) ? console.log(`😊😊😊 Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`) :
+    console.log(`🤬🤬🤬 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
+};
+
+/* Function will keep collecting items from a provided array until the callback provided returns a truthy value 
+Returns an array of the collected items */
+
+const takeUntil = (array, callback) => {
+  let newArray = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return newArray;
+    } else {
+      newArray.push(item);
+    }
+  }
+};
+
+// Test code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); // => [ 1, 2, 5, 7, 2 ]
+console.log('---');
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2); // => [ 'I\'ve', 'been', 'to', 'Hollywood' ]
