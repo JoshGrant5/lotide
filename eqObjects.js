66b14e8a79addcd953365f33659b998432a4e060
JoshGrant5 <joshgg@icloud.com>
2020-09-18 21:24:59 +0000

    Refactored using recursion

--- eqObjects.js
+++ eqObjects.js
@@ -11,6 +11,24 @@ const eqArrays = (array1, array2) => {
 };
 
 // Take in two objects and return true or false, based on a perfect match
+// const eqObjects = (object1, object2) => {
+//   if (Object.keys(object1).length !== Object.keys(object2).length) {
+//     return false;
+//   } else {
+//     for (let key in object1) {
+//       if (!Object.keys(object2).includes(key)) { // are the keys the same for both objects?
+//         return false;
+//       }
+//       if (Array.isArray(object1[key])) { // is the value an array?
+//         if (!eqArrays(object1[key], object2[key])) { // if so, does this array match with the value of object2?
+//           return false;
+//         }
+//       }
+//     }
+//   }
+//   return true;
+// };
+
 const eqObjects = (object1, object2) => {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
@@ -19,9 +37,15 @@ const eqObjects = (object1, object2) => {
       if (!Object.keys(object2).includes(key)) { // are the keys the same for both objects?
         return false;
       }
-      if (Array.isArray(object1[key])) { // is the value an array?
-        if (!eqArrays(object1[key], object2[key])) { // if so, does this array match with the value of object2?
-          return false;
+      if (typeof object1[key] === 'object') {
+        if (Array.isArray(object1[key])) { // is the value an array?
+          if (!eqArrays(object1[key], object2[key])) { // if so, does this array match with the value of object2?
+            return false;
+          }
+        } else {
+          if (!eqObjects(object1[key], object2[key])) {
+            return false;
+          }
         }
       }
     }
@@ -47,3 +71,9 @@ console.log(eqObjects(cd, cd2)); // => false
 const ef = { c: "1", b: ["2", 3] };
 const fe = { d: ["2", 3], c: "1" };
 console.log(eqObjects(ef, fe)); // => false
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+console.log(eqObjects({ a: [{z: 1}], b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: {b: 1}, b: { z: 2, y: 4, a: [1,2] }}, { a: { b: 1 }, b: { y: 4, a: [1,2], z: 2 }})); // => true
