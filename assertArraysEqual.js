0090eee3d8a4b4b27e1c9e3f78e2ea54fd5126f4
JoshGrant5 <joshgg@icloud.com>
2020-09-18 21:41:36 +0000

    Refactored using recursion (for eqArrays)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,14 @@ const eqArrays = (array1, array2) => {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
   }
   return true;
@@ -17,8 +23,13 @@ const assertArraysEqual = (array1, array2) => {
 };
 
 // Test code
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => false
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => false
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertArraysEqual([[2, 3], [4]], [[2, 3], [4]], true); // => true
+assertArraysEqual([[2, 3], [4]], [[2, 3], [4, 5]], false); // => false
+assertArraysEqual([[2, 3], [4]], [[2, 3], 4], false); // => false
+assertArraysEqual([[2, 3], [4, 5, [6, 7, 8]]], [[2, 3], [4, 5, [6, 7, 8]]], false); // => true
